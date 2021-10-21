d2b68e05147f79867c9e2735aba1e1e0d080c951
Lautaro Nasello <lautaronasello@gmail.com>
2021-10-21 14:06:50 -0300

    changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Provider } from 'react-redux';
 import Home from './components/Home';
+import Nav from './components/Nav';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
+      <Nav />
       <Home />
     </Provider>
   );
